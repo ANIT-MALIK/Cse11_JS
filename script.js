c2b2cea79a0d3532c34dd547142edadc79bbe5d5
anit malik <anit.25b01011180@abes.ac.in>
2026-02-24 10:13:06 +0530

    Add HTML and JS files

--- script.js
+++ script.js
@@ -15,3 +15,13 @@ console.log(typeof(num));
 var sayHello = function(){
     alert('hello world!');
 }
+const sym1 = Symbol(4)
+console.log(sym1)
+const sym2 = Symbol(4)
+console.log(sym2)
+if(sym1==sym2){
+    console.log("true")
+}
+else{
+    console.log("false")
+}
