01fb85052ec40b837fddec39704caf3d65745ca6
anit malik <anit.25b01011180@abes.ac.in>
2026-02-24 09:28:17 +0530

    datatypes

---
+++ script.js
@@ -0,0 +1,14 @@
+console.log("first")
+
+var num=10;
+console.log(num);
+console.log(typeof(num));
+num = false;
+console.log(num);
+console.log(typeof(num));
+num = null;
+console.log(num);
+console.log(typeof(num));
+num = undefined;
+console.log(num);
+console.log(typeof(num));
